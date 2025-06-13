dc5f2a008184d1cf0ae43d5f3545079285242edd
unknown <muhammadfathir.p@gmail.com>
2025-06-13 11:13:49 +0700

    Released V2.0

--- TapQRSMTI.ino
+++ TapQRSMTI.ino
@@ -3,12 +3,17 @@
 #include <HTTPClient.h>
 #include <Wiegand.h>
 #include <ArduinoJson.h>
+#include <ESPmDNS.h>
+#include <NetworkUdp.h>
+#include <ArduinoOTA.h>
 // These are the pins connected to the Wiegand D0 and D1 signals.
-#define PIN_D0 22   
-#define PIN_D1 23
-#define BEEPER 14   
-#define LED 12
-#define OUT 2
+#define PIN_D0 12   
+#define PIN_D1 14
+#define BEEPER 27   
+#define LED 26
+#define OUT 13
+#define espON1 25
+#define espON2 33
 
 // The object that handles the wiegand protocol
 Wiegand wiegand;
@@ -22,8 +27,9 @@ bool gateOpen = false;
 bool gateOpened = false;
 bool noDataFound = false;
 bool comError = false;
+bool getError = false;
 bool grantedBeep = false;
-
+bool statusState = false;
 String waktu = "";
 String postResponse = "";
 String userID = "";
@@ -35,6 +41,8 @@ const char* ssid = "SMTI-PRO";
 const char* password = "";
 const char* waktuAPI = "http://192.168.1.199:1881/waktu";
 const char* sendDataAPI = "http://192.168.1.199:1881/masuk";
+
+const char* hostname = "SMTI Gate Entry";
 
 String getRequest(const char* serverUrl) {
   if (WiFi.status() != WL_CONNECTED) {
@@ -53,7 +61,7 @@ String getRequest(const char* serverUrl) {
   Serial.print("[HTTP] GET...\n");
   // start connection and send HTTP GET request
   int httpCode = http.GET();
-
+  
   // httpCode will be negative on error
   if (httpCode > 0) {
     // HTTP header has been sent and server response header has been handled
@@ -64,11 +72,16 @@ String getRequest(const char* serverUrl) {
       payload = http.getString();
       Serial.println("Response Payload:");
       Serial.println(payload);
+      getError = false;
     } else {
       Serial.printf("[HTTP] Server responded with error code: %d\n", httpCode);
+      getError = true;
+      timer1 = currentTime;
     }
   } else {
     Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+    getError = true;
+    timer1 = currentTime;
   }
 
   http.end(); // Free the resources
@@ -77,17 +90,58 @@ String getRequest(const char* serverUrl) {
 
 void setup() {
   Serial.begin(115200);
+  WiFi.setHostname(hostname);
+  WiFi.begin(ssid, password); 
 
-  WiFi.begin(ssid, password);
-  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
-    Serial.println("Connection Failed! Rebooting...");
-    delay(2000);
-    ESP.restart();
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print("."); // Print a dot to show activity
+    digitalWrite(2, HIGH); // Turn LED on
+    delay(500); // Short delay for blink effect
+    digitalWrite(2, LOW); // Turn LED off
+    delay(500); // Short delay for blink effect
   }
   Serial.println("\nWiFi connected!");
   Serial.print("IP Address: ");
   Serial.println(WiFi.localIP());
   
+  ArduinoOTA.setHostname(hostname);
+  ArduinoOTA.setPasswordHash("83eaebf5769ba0437dae13a2c0d1a1ea");
+
+  ArduinoOTA
+    .onStart([]() {
+      String type;
+      if (ArduinoOTA.getCommand() == U_FLASH) {
+        type = "sketch";
+      } else {  // U_SPIFFS
+        type = "filesystem";
+      }
+
+      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+      Serial.println("Start updating " + type);
+    })
+    .onEnd([]() {
+      Serial.println("\nEnd");
+    })
+    .onProgress([](unsigned int progress, unsigned int total) {
+      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    })
+    .onError([](ota_error_t error) {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) {
+        Serial.println("Auth Failed");
+      } else if (error == OTA_BEGIN_ERROR) {
+        Serial.println("Begin Failed");
+      } else if (error == OTA_CONNECT_ERROR) {
+        Serial.println("Connect Failed");
+      } else if (error == OTA_RECEIVE_ERROR) {
+        Serial.println("Receive Failed");
+      } else if (error == OTA_END_ERROR) {
+        Serial.println("End Failed");
+      }
+    });
+
+  ArduinoOTA.begin();
+
   wiegand.onReceive(receivedData, "Card readed: ");
   wiegand.onReceiveError(receivedDataError, "Card read error: ");
   wiegand.onStateChange(stateChanged, "State changed: ");
@@ -101,10 +155,15 @@ void setup() {
   pinStateChanged();
 
   pinMode(OUT,OUTPUT);
+  pinMode(2,OUTPUT);
   pinMode(LED,OUTPUT);
   pinMode(BEEPER,OUTPUT);
-  digitalWrite(LED,HIGH);
-  digitalWrite(BEEPER,HIGH);
+  pinMode(espON1,OUTPUT);
+  pinMode(espON2,OUTPUT);
+  digitalWrite(espON1,LOW);
+  digitalWrite(espON2,LOW);
+  digitalWrite(LED,LOW);
+  digitalWrite(BEEPER,LOW);
 }
 
 void loop() {
@@ -123,43 +182,72 @@ void loop() {
       waktu = doc["waktu"].as<String>();
       Serial.print("Waktu : ");
       Serial.println(waktu);
+      if(!getError){
       sendPostRequest(waktu,hexString);
+      }
+      else {
+        comError = true;
+        timer1 = currentTime;
+      }
     }
     oldHexString = hexString;
     hexString = "";
   }
-  gateControl();
+  if(WiFi.status() == WL_CONNECTED){
+    gateControl();
+  }
+  else{
+    if(currentTime - timer1 >= 500){
+      timer1 = currentTime;
+      statusState = !statusState;
+      digitalWrite(2, statusState);
+    }
+  }
 }
 
 void gateControl(){
   if(gateOpen){
   digitalWrite(OUT,LOW);
-  digitalWrite(LED,LOW);
+  digitalWrite(2,LOW);
+  digitalWrite(LED,HIGH);
   gateOpened = true;
   }
   else{
     digitalWrite(OUT,HIGH);
-    digitalWrite(LED,HIGH);
+    digitalWrite(2,HIGH);
+    digitalWrite(LED,LOW);
     gateOpened = false;
   }
   if(gateOpened){
     if(currentTime - timer1 >= 1000)gateOpen = false;
   }
   if(noDataFound){
-    digitalWrite(BEEPER,LOW);
-    if(currentTime - timer1 >= 1000){
-      digitalWrite(BEEPER,HIGH);
+    digitalWrite(BEEPER,HIGH);
+    Serial.println("NO DATA FOUND");
+    if(currentTime - timer1 >= 1000 && currentTime - timer1 < 2010){
+      digitalWrite(BEEPER,LOW);
       noDataFound = false;
     }
   }
   if(comError){
-    digitalWrite(BEEPER,LOW);
-    if(currentTime - timer1 >= 500){
+    if(currentTime - timer1 < 100){
       digitalWrite(BEEPER,HIGH);
+    }
+    Serial.println("Communication Error");
+    if(currentTime - timer1 >= 100 && currentTime - timer1 < 400){
+      digitalWrite(BEEPER,LOW);
+    }
+    if(currentTime - timer1 >= 400 && currentTime - timer1 < 500){
+      digitalWrite(BEEPER,HIGH);
+      comError = false;
+    }
+    if(currentTime - timer1 >= 600){
+      digitalWrite(BEEPER,LOW);
       comError = false;
     }
 
   }
+  if(!comError && !noDataFound)digitalWrite(BEEPER,LOW);
 }
 
 void sendPostRequest(String waktu, String data) {
@@ -184,13 +272,15 @@ void sendPostRequest(String waktu, String data) {
   Serial.println(httpRequestData);
 
   // Set timeouts to prevent hanging and Watchdog Timer resets
-  http.setConnectTimeout(5000); // 5 seconds
-  http.setTimeout(8000); // 8 seconds
+  http.setConnectTimeout(2000); // 5 seconds
+  http.setTimeout(2500); // 8 seconds
 
   Serial.print("[HTTP] performing POST request...\n");
   int httpResponseCode = http.POST(httpRequestData);
 
   if (httpResponseCode > 0) {
+    getError = false;
+    timer1 = currentTime;
     Serial.printf("[HTTP] POST Response code: %d\n", httpResponseCode);
     String payload = http.getString();
     Serial.println("POST Payload:");
@@ -203,7 +293,6 @@ void sendPostRequest(String waktu, String data) {
     if(postResponse == "Accepted"){
       Serial.println("Accepted");
       gateOpen = true;
-      timer1 = currentTime;
     }
     else{
       Serial.println("Refused");
